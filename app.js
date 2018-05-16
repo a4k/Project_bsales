b581e26f2ec5ced25f5b72c39ffcc83e081529f5
a4kosen <vsekanal@gmail.com>
2018-05-16 15:36:17 +0400

    Add Page Products

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');;
+const productRouter = require('./routes/product');
 
 var app = express();
 
@@ -28,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
