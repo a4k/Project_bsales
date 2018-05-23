145867ec47f53554eab40f972335e02a1017d95a
a4kosen <vsekanal@gmail.com>
2018-05-23 17:41:26 +0400

    Add Page Product

---
+++ source/js/app.js
@@ -0,0 +1,12 @@
+var holder_image = document.getElementByClassName('holder_image');
+
+Holder.run({
+	themes: {
+		'simple': {
+			bg: "#fff",
+			fg: '#000',
+			size: 12
+		}
+	},
+  images: holder_image
+});
