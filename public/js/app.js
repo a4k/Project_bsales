c349db99d1344f9060c375eee44e38a19f40e1a8
Никита Бровкович <n.brovkovich@rsit.ru>
2018-06-08 11:14:14 +0400

    Make blocks services, company, clients

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ $('[class=toggles]').find('.toggle_btn, h3').click(function(){
 });
 
 
-
+// Открытие модального окна
 $('[class=modal]').find('.close, .modal_shadow').click(function(){
 	var name = $(this).attr('data-name');
 	$('#'+name).fadeOut();
@@ -13,9 +13,64 @@ $('#openNewHire').click(function(){
 	var title = $(this).attr('data-title');
 	$('#newHire').fadeIn();
 	$('#newHire').find('.title-info').text(title);
+});
+
+// Выбор главного цвета на изображении
+$('#main_products .cards').find('.card').each(function(){
+	$(this).hover(() => {
+		let c = $(this).attr('data-color');
+		$(this).css({'background': c});
+	}, () => {
+		$(this).css({'background': '#fff'})
+	})
 })
 
-
+// Выбор главного цвета на изображении
+function getColor(imgEl) {
+    
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+        
+    if (!context) {
+        return defaultRGB;
+    }
+    
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    
+    context.drawImage(imgEl, 0, 0);
+    
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */alert('x');
+        return defaultRGB;
+    }
+    
+    length = data.data.length;
+    
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+    
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+    
+    return rgb;
+    
+}
 
 // Реализация слайдера
 class Slider {
